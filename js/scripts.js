e72d81710fc89de3bee77d38172ba7054b5d055b
jared beckler and nic netzel <netzeln@gmail.com>
2016-01-21 15:29:33 -0800

    add spec for splitting to array

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,18 @@ var toRomanNumeral = function(userInput){
   var i = userInput;
 
   if (i < 4000){
-    return true;
+    return toArray(userInput);
   }
   return false;
+}
 
+var toArray = function(userInput){
+  var splitableNumber = userInput.toString();
+  console.log(userInput);
+  console.log(splitableNumber);
+  var splitNumber = splitableNumber.split("");
+
+  return splitNumber;
 }
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,13 @@
 describe("toRomanNumeral", function(){
+
   it("will see if a number is convertable to roman numerals", function(){
-  expect(toRomanNumeral(4001)).to.equal(false);
+    expect(toRomanNumeral(4001)).to.equal(false);
+  });
+
+});
+
+describe("toArray", function(){
+  it("will separate the input number", function(){
+    expect(toArray(3999)).to.eql(["3","9","9","9"]);
   });
 });
